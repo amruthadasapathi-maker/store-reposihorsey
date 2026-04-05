23563a5e8884c236ee3bab93f26283901701d909
Rahul@The_Great <amruthadasapathi@gmail.com>
2026-04-05 15:05:31 +0530

    Update install.sh

--- Tetris/install.sh
+++ Tetris/install.sh
@@ -6,14 +6,14 @@ TMPDIR=/mnt/us/KFPM-Temporary
 mkdir -p "$TMPDIR"
 
 # Download + Extract
-curl -fSL --progress-bar -o "$TMPDIR/kanki.zip" https://github.com/crizmo/kanki/releases/latest/download/kanki.zip
-unzip -q "$TMPDIR/kanki.zip" -d "$TMPDIR"
+curl -fSL --progress-bar -o "$TMPDIR/KShips.zip" https://github.com/LOT-PL/KShips/releases/latest/download/KShips.zip
+unzip -q "$TMPDIR/KShips.zip" -d "$TMPDIR"
 
 # Copy Contents
-mkdir -p /mnt/us/documents/kanki
-cp -r "$TMPDIR/kanki"/* /mnt/us/documents/kanki/
-cp "$TMPDIR/kanki.sh" /mnt/us/documents/
-chmod +x /mnt/us/documents/kanki.sh
+mkdir -p /mnt/us/documents/KShips
+cp -r "$TMPDIR/KShips"/* /mnt/us/documents/KShips/
+cp "$TMPDIR/KShips.sh" /mnt/us/documents/
+chmod +x /mnt/us/documents/KShips.sh
 
 # Cleanup
 rm -rf "$TMPDIR"
